522049fe0d51e56ca703425e5e7dbe8409901c6f
gitwillian <willian.siqueiras@gmail.com>
2013-11-27 21:55:51 -0200

    Update JenkinsGit.java

--- src/jenkinsgit/JenkinsGit.java
+++ src/jenkinsgit/JenkinsGit.java
@@ -8,7 +8,7 @@ package jenkinsgit;
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 import org.apache.commons.io.FileUtils;
-import org.openqa.selenium.By;
+
 
 
 import org.openqa.selenium.firefox.FirefoxDriver;
